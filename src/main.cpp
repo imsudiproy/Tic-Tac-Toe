8cde6c39bcc43bd750d235edd04b3751e3bf9bc3
Sudip Roy <sudiproy1999@gmail.com>
2024-09-28 13:19:39 +0530

    add all functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,44 @@
 #include "header.h"
 #include "common.h"
 
+int currentPlayer = 1; // 1 for Player X, 2 for Player O
+char currentMarker;
+bool gameWon = false;
 char board[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 void displayBoard();
+bool checkWin();
+bool checkDraw();
+void switchPlayer(int, char);
+bool placeMarker(int, char);
 
 int main(){
     std::cout<< "\n **Welcome to Tic-Tac-Toe** \n\n";
-    displayBoard();
+    std::cout << "Player 1: X,   Player 2: O\n";
+    char currentMarker = 'X';
+
+    while(true) {
+        displayBoard();
+        int slot;
+
+        std::cout<<"Player "<<currentPlayer<<"enter your move(1-9): ";
+        std::cin>>slot;
+        if(!placeMarker(slot, currentMarker)){
+            std::cout<<"Invalid Move!! Try again\n";
+            continue;
+        }
+
+        //check if the player has won
+        if(checkWin()){
+            std::cout<<"Player "<<currentPlayer<<" is the Winner\n";
+            break;
+        }
+
+        if(checkDraw){
+            std::cout<<"Draww\n";
+            break;;
+        }
+        switchPlayer(currentPlayer, currentMarker);
+    }
+
+    return 0;
 }
